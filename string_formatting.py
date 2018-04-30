1c3a0f68aa769e5eaf7317f5636b627ca11b57f4
jstrickler <jstrickler@gmail.com>
2018-04-30 15:08:10 -0500

    print and if

--- string_formatting.py
+++ string_formatting.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python
+
+x = 5.9
+y = 1.187
+
+result = x / y
+
+print("result is", result)
+
+print("result is {:.2f}".format(result))
+
+a = 124
+b = 99
+
+
+print("{:05d}".format(a))
+print("{:05d}".format(b))
+
+print("%05d" % (b))
+
+print(f"{b:05d}")
+
+cave_man = 'Barney Rubble'
+city = 'Bedrock'
+
+print(f"{cave_man} lives in {city}")
+
+
---
+++ user_input.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+knight_name = input("What is your name, good sir Knight? ")
+
+print("Knight's name is {}".format(knight_name))
