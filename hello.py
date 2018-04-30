9ed75783dae54ed5ad5fc00899dff13f4fe96b7c
jstrickler <jstrickler@gmail.com>
2018-04-30 12:52:09 -0500

    added wombat.py and .gitignore

--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
 #!/usr/bin/env python
+
 print("Hello, Dillard's world")
+print("A beautiful day in Little Rock")
+
---
+++ wombat.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+print("Wombats are great!")
+
+
+x = 5
+y = 20
+result = x + y
+print(result)
+
+x_value = 5
+y_value = 20
+
+print(type(x))
+cave_man = 'Barney Rubble'
+print(type(cave_man))
+
+document_type = None
+
+class_for_trip = 'Sophomore'
+bomb_yield = '98 kilotons'
+
+location_dir = 'North'
+max_kumquats = 37
+user_id = 'jstrickler'
+
+
+x = 10
+y = "20"
+
+# result = x + y
+
+print(str(x) + y)
+print(x + int(y))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
